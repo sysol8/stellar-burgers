b2ff72548171394ed96e4eff2f8fb2cb70ca218a
sysol8 <iabrosimov@list.ru>
2025-10-16 17:00:31 +0300

    добавил дата-атрибут для элемента с названием ингредиента

--- src/components/ui/burger-ingredient/burger-ingredient.tsx
+++ src/components/ui/burger-ingredient/burger-ingredient.tsx
@@ -31,7 +31,12 @@ export const BurgerIngredientUI: FC<TBurgerIngredientUIProps> = memo(
             <p className='text text_type_digits-default mr-2'>{price}</p>
             <CurrencyIcon type='primary' />
           </div>
-          <p className={`text text_type_main-default ${styles.text}`}>{name}</p>
+          <p
+            className={`text text_type_main-default ${styles.text}`}
+            data-cy='ingredient-name'
+          >
+            {name}
+          </p>
         </Link>
         <AddButton
           text='Добавить'
