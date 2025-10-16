926f52f9b453f0b72e2a96dc3097a564ff437d0e
sysol8 <iabrosimov@list.ru>
2025-10-16 17:02:08 +0300

    добавил кастомные команды для заполнения конструктора бургера и вытаскивания названия ингредиента

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,4 +14,44 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+
+type BunSelector = string;
+type IngredientSelector = string;
+type BunOrIngredient = BunSelector | IngredientSelector;
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      fillBurgerConstructor(
+        buns: string,
+        ingredients: string
+      ): Chainable<JQuery<HTMLElement>>;
+      getIngredientName(nameSelector?: string): Chainable<string>;
+    }
+  }
+}
+
+Cypress.Commands.add(
+  'fillBurgerConstructor',
+  (buns: BunSelector, ingredients: IngredientSelector) => {
+    cy.get(buns).first().find('button').click();
+
+    cy.get(ingredients).each((card: any, i: number) => {
+      if (i >= 5) return false;
+      cy.wrap(card).scrollIntoView().find('button').click();
+    });
+  }
+);
+
+Cypress.Commands.add(
+  'getIngredientName',
+  { prevSubject: ['element'] },
+  (subject, nameSelector: string = '[data-cy=ingredient-name]') => {
+    return cy
+      .wrap(subject)
+      .find(nameSelector)
+      .invoke('text')
+      .then((text: string) => text.trim());
+  }
+);
